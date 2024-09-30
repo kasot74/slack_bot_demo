dce52bec1c6132f61526be04e8b15d9cc6513c9d
kasot74 <hcsds0123@gmail.com>
2024-09-30 18:04:37 +0800

    update security

--- src/bot_main.py
+++ src/bot_main.py
@@ -2,11 +2,11 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from .handlers import register_handlers
 from .utilities import read_config
-from .database import db
+from .database import con_db
 
 # 從配置文件中讀取 tokens
 config = read_config('config/config.txt')
-
+db = con_db(config)
 # 初始化 Slack App
 app = App(token=config['SLACK_BOT_TOKEN'], signing_secret=config['SLACK_SIGNING_SECRET'])
 
--- src/database.py
+++ src/database.py
@@ -1,5 +1,14 @@
 from pymongo import MongoClient
 
-# MongoDB 連線
-client = MongoClient("mongodb://localhost:27018/")
-db = client.myDatabase
+#創建連線
+def con_db(config):    
+    print(config)
+    user=config['MONGO_USER']
+    pas=config['MONGO_USER']
+    host=config['MONGO_HOST']
+    port=config['MONGO_PORT']
+    con_str=f"mongodb://{user}:{pas}@{host}:{port}/?authMechanism=SCRAM-SHA-1&authSource=admin"
+    # MongoDB 連線
+    client = MongoClient(con_str)
+    db = client.myDatabase
+    return db
