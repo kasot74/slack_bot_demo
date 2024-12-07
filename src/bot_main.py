1ede8322ccd28d86099ee68f6dee15a4d3d59568
kasot74 <hcsds0123@gmail.com>
2024-12-07 13:12:23 +0800

    取消LOG add images/人生.jpg

--- src/bot_main.py
+++ src/bot_main.py
@@ -4,10 +4,8 @@ from .handlers import register_handlers
 from .utilities import read_config
 from .database import con_db
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    filename='app.log',  # 輸出到 app.log 檔案
-                    filemode='w')  # 'w' 會覆蓋檔案，'a' 會追加到檔案末尾
+
+#logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', filename='app.log',  # 輸出到 app.log 檔案 filemode='w')  # 'w' 會覆蓋檔案，'a' 會追加到檔案末尾
 
 # 從配置文件中讀取 tokens
 config = read_config('config/config.txt')
