9efee34ee805646dc2a9e461bc6094f89a90503f
herry <hcsds0123@gmail.com>
2024-12-23 10:10:31 +0800

    add

--- src/handlers.py
+++ src/handlers.py
@@ -67,12 +67,12 @@ def register_handlers(app, config, db):
 
 
     # !add 指令
-    @app.message(re.compile(r"^!add\s+(.+)\s+(.+)", re.DOTALL))
+    @app.message(re.compile(r"^!add\s+(\S+)\s\s+(.+)", re.DOTALL))
     def handle_add_message(message, say):
-        match = re.match(r"^!add\s+(.+)\s+(.+)", message['text'], re.DOTALL)
+        match = re.match(r"^!add\s+(\S+)\s\s+(.+)", message['text'], re.DOTALL)
         if match:
-            msg_text = match.group(1).strip()
-            response_text = match.group(2).strip().replace('\\n', '\n')
+            msg_text = match.group(1).strip()        # 第一個空白後的關鍵字
+            response_text = match.group(2).strip()    # 第二個空白後的回應內容
             add_commit(msg_text, response_text, say)
 
     # !show 指令
