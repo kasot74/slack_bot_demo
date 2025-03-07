a8fd0363fcff940b5c31700808ee6eb67644f587
herry <hcsds0123@gmail.com>
2025-03-07 13:47:01 +0800

    fix

--- src/handlers.py
+++ src/handlers.py
@@ -152,7 +152,7 @@ def register_handlers(app, config, db):
                 if random.random() < 0.7:  # 70%的機率釣到檔案
                     # 隨機選取一個或多個檔案
                     selected_files = random.sample(quotes, k=min(1, len(quotes)))  # k=2 表示最多選取2個
-                    send_image(channel, doc['say'],f"你釣到了：{', '.join(selected_files)}", os.path.join('fishpond',selected_files))                                
+                    send_image(channel,f"你釣到了：{', '.join(selected_files)}", os.path.join('fishpond',selected_files))                                
                 else:
                     # 30%的機率沒釣到
                     say("很遺憾，你什麼也沒釣到！")
