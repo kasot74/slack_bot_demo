a354a4364d6b5887908019494c13eec8ac4b03a3
kasot74 <hcsds0123@gmail.com>
2025-04-15 22:13:52 +0800

    fix config name

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 #創建連線
 def con_db(config):        
     user=config['MONGO_USER']
-    pas=config['MONGO_USER']
+    pas=config['MONGO_PASSWORD']
     host=config['MONGO_HOST']
     port=config['MONGO_PORT']
     con_str=f"mongodb://{user}:{pas}@{host}:{port}/?authMechanism=SCRAM-SHA-1&authSource=admin"
