5f3804a668a0e58659d150f0107db258981ca435
herry <hcsds0123@gmail.com>
2025-08-05 15:01:55 +0800

    update painting

--- src/model/stability_model.py
+++ src/model/stability_model.py
@@ -6,7 +6,8 @@ import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 from io import BytesIO
 from PIL import Image
 from stability_sdk import client
-from ..AI_Service.xai import painting
+#from ..AI_Service.xai import painting
+from ..AI_Service.openai import painting
 from ..utilities import read_config
 # 從配置文件中讀取 tokens
 config = read_config('config/config.txt')
