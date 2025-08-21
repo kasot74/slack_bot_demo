aa19e5313ef9d6479471a6ec268dbf3082ed4ab2
herry <hcsds0123@gmail.com>
2025-08-21 12:17:50 +0800

    update

--- src/AI_Service/gemini.py
+++ src/AI_Service/gemini.py
@@ -221,7 +221,11 @@ def create_video(prompt, negative_prompt="", max_wait_time=300, image_bytes=None
             mime_type = kind.mime  # 例如：image/jpeg 或 image/png
 
             # 建立 GenAI 圖片物件
-            image = types.image.Image(data=image_bytes, mime_type=mime_type)
+            image =  genai.upload_file(
+                display_name="x",
+                mime_type=mime_type,
+                data=image_bytes
+            )
             print("📷 使用上傳的圖片")
         
         # 配置影片生成參數
